1b3e25d40b18f504e87c51191fadde6888e6ff5e
Kang BS <kbsoo7521@gmail.com>
2023-01-09 17:24:15 +0900

    2 code 수정

--- iris_classification.py
+++ iris_classification.py
@@ -1,3 +1,12 @@
+from sklearn.datasets import load_iris
+import numpy as np
 import pandas as pd
 
-df = pd.read_csv('iris.csv')
+iris = load_iris()
+iris = dict(iris)
+
+iris_data = pd.DataFrame(data=np.c_[iris['data'], iris['target']],
+                         columns=iris['feature_names'] + ['target'])
+
+file_name = f"Iris.csv"
+iris_data.to_csv(file_name)
